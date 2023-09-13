87e45d35057048329719634adb727be7bb857c76
rm <xyzeusrm@gmail.com>
2023-09-13 09:50:23 -0700

    Implemented initial search result

--- src/pages/content/components/SearchPrompt.tsx
+++ src/pages/content/components/SearchPrompt.tsx
@@ -2,6 +2,7 @@ import {
   Calculator,
   Calendar,
   CreditCard,
+  MessageSquare,
   Search,
   Settings,
   Smile,
@@ -94,39 +95,14 @@ export function SearchPrompt() {
       <Command className="border rounded-lg shadow-md">
         <SearchForm onSubmit={handleSeachSubmit} />
         <CommandList className="max-h-[500px]">
-          {/* <CommandEmpty>No results found.</CommandEmpty>
-          <CommandGroup heading="Suggestions">
-            <CommandItem>
-              <Calendar className="w-4 h-4 mr-2" />
-              <span>Calendar</span>
-            </CommandItem>
-            <CommandItem>
-              <Smile className="w-4 h-4 mr-2" />
-              <span>Search Emoji</span>
-            </CommandItem>
-            <CommandItem>
-              <Calculator className="w-4 h-4 mr-2" />
-              <span>Calculator</span>
-            </CommandItem>
+          <CommandGroup heading="Conversations">
+            {searchResult.conversations.map((conversation) => (
+              <CommandItem key={conversation.id}>
+                <MessageSquare className="w-4 h-4 mr-2" />
+                <span>{conversation.title}</span>
+              </CommandItem>
+            ))}
           </CommandGroup>
-          <CommandSeparator />
-          <CommandGroup heading="Settings">
-            <CommandItem>
-              <User className="w-4 h-4 mr-2" />
-              <span>Profile</span>
-              <CommandShortcut>⌘P</CommandShortcut>
-            </CommandItem>
-            <CommandItem>
-              <CreditCard className="w-4 h-4 mr-2" />
-              <span>Billing</span>
-              <CommandShortcut>⌘B</CommandShortcut>
-            </CommandItem>
-            <CommandItem>
-              <Settings className="w-4 h-4 mr-2" />
-              <span>Settings</span>
-              <CommandShortcut>⌘S</CommandShortcut>
-            </CommandItem>
-          </CommandGroup> */}
         </CommandList>
       </Command>
     </CommandDialog>
