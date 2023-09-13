394d81c2a6a3b13231cf9e9ddc13786343a32fe1
rm <xyzeusrm@gmail.com>
2023-09-13 06:09:10 -0700

    Updated root id

--- src/pages/content/entry.tsx
+++ src/pages/content/entry.tsx
@@ -2,11 +2,12 @@ import { createRoot } from "react-dom/client";
 import App from "@src/pages/content/app";
 import refreshOnUpdate from "virtual:reload-on-update-in-view";
 import { Provider } from "jotai";
+import { CONTENT_VIEW_CONTAINER_ID } from "@src/constants";
 
 refreshOnUpdate("pages/content");
 
 const root = document.createElement("div");
-root.id = "chatgpt-archive-extension-root";
+root.id = CONTENT_VIEW_CONTAINER_ID;
 document.querySelector("html").appendChild(root);
 
 if (window.location.pathname.includes("auth/login")) {
