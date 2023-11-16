2d4f6c75818f96fdd1efbe31cb04e3716a3a09fd
rm <xyzeusrm@gmail.com>
2023-11-15 18:52:58 -0800

    Updated thumb style

--- src/pages/content/components/Thumb.tsx
+++ src/pages/content/components/Thumb.tsx
@@ -3,34 +3,15 @@ import { useAtom } from "jotai";
 import { panelOpenAtom } from "../context";
 import { SpinnerIcon } from "@src/components/Spinner";
 import { useRefresh } from "../hook";
+import { Logo } from "@src/components/Logo";
 
 export function Thumb() {
   const [open, setOpen] = useAtom(panelOpenAtom);
   const { refreshing } = useRefresh();
 
   return (
-    <div>
-      <button
-        className="hover:opacity-80 trans"
-        style={{
-          width: "50px",
-          height: "40px",
-          position: "fixed",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          top: "80px",
-          right: "0",
-          cursor: "pointer",
-          transform: open ? "translateX(100%)" : "translateX(0%)",
-          transition: "all 0.3s ease-out",
-          outline: "none",
-          border: "none",
-          boxShadow: "0 0 10px rgba(0,0,0,0.2)",
-          background: "hsl(var(--background))",
-        }}
-        onClick={() => setOpen(!open)}
-      >
+    <div className="thumb" data-state={open ? "hidden" : "show"}>
+      <div role="button" className="thumb-item" onClick={() => setOpen(!open)}>
         {refreshing ? (
           <SpinnerIcon size={20} />
         ) : (
@@ -41,8 +22,14 @@ export function Thumb() {
               height: "20px",
             }}
           />
+          // <Logo
+          //   style={{
+          //     width: "28px",
+          //     height: "28px",
+          //   }}
+          // />
         )}
-      </button>
+      </div>
     </div>
   );
 }
